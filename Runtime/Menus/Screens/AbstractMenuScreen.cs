29757f078361e24f11c30d78f7295994714e61e2
Hyago Oliveira <hyagogow@gmail.com>
2025-08-15 11:16:20 -0300

    Add AbstractMenuScreen

---
+++ Runtime/Menus/Screens/AbstractMenuScreen.cs
@@ -0,0 +1,15 @@
+namespace ActionCode.UISystem
+{
+    /// <summary>
+    /// Abstract controller for a generic Menu Screen.
+    /// <para>
+    /// Menu Screens are used to display different parts of a Menu, as a sub-menu or a specific section.
+    /// </para>
+    /// </summary>
+    public abstract class AbstractMenuScreen : AbstractController
+    {
+        public AbstractMenu Menu { get; private set; }
+
+        protected virtual void Initialize(AbstractMenu menu) => Menu = menu;
+    }
+}
