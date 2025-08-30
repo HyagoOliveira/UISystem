a8277adb724b052533ce157e803a18a8630ee258
Hyago Oliveira <hyagogow@gmail.com>
2025-08-30 20:59:36 -0300

    Add ScaleAnimator

---
+++ Runtime/Animators/ScaleAnimator.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+namespace ActionCode.UISystem
+{
+    /// <summary>
+    /// Scale animator for a Visual Elements.
+    /// <para>
+    /// Use the <see cref="scaleCurve"/> curve to animate the Visual Element Transform Scale.
+    /// </para>
+    /// </summary>
+    public sealed class ScaleAnimator : AbstractAnimator
+    {
+        [Space]
+        [SerializeField, Tooltip("The curve driving the scale animation.")]
+        private AnimationCurve scaleCurve;
+
+        public void SetScale(float scale) => Element.transform.scale = Vector3.one * scale;
+
+        protected override void UpdateAnimation()
+        {
+            CurrentTime += Time.deltaTime * Speed;
+            SetScale(scaleCurve.Evaluate(CurrentTime));
+            CheckStopCondition();
+        }
+
+        private void CheckStopCondition()
+        {
+            if (HasCurveFinished(scaleCurve, CurrentTime)) Stop();
+        }
+    }
+}
