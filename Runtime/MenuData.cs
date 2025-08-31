807199997d00fbe4d429a45ff9527475ee0f3e50
Hyago Oliveira <hyagogow@gmail.com>
2025-08-31 11:24:03 -0300

    Add Popup AudioClips

--- Runtime/MenuData.cs
+++ Runtime/MenuData.cs
@@ -8,12 +8,19 @@ namespace ActionCode.UISystem
     [CreateAssetMenu(fileName = "MenuData", menuName = "ActionCode/UI System/Menu Data", order = 110)]
     public class MenuData : ScriptableObject
     {
+        [Header("Menus")]
         [Tooltip("The audio played when selected.")]
         public AudioClip selection;
         [Tooltip("The audio played when submitted.")]
         public AudioClip submit;
         [Tooltip("The audio played when canceled.")]
         public AudioClip cancel;
+
+        [Header("Popus")]
+        [Tooltip("The audio played when opening a Popup.")]
+        public AudioClip openPopup;
+        [Tooltip("The audio played when closing a Popup.")]
+        public AudioClip closePopup;
 
         public float GetSubmitTime() => submit.length;
     }
