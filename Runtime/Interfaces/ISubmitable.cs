ef7cb20fe17917667a6fa31baa2bf4af044d2512
Hyago Oliveira <hyagogow@gmail.com>
2026-03-07 20:00:11 -0300

    Improve ISubmitable

--- Runtime/Interfaces/ISubmitable.cs
+++ Runtime/Interfaces/ISubmitable.cs
@@ -1,11 +1,12 @@
 using System;
+using UnityEngine.EventSystems;
 
 namespace ActionCode.UISystem
 {
     /// <summary>
     /// Interface used on objects able to be submitted (like buttons). 
     /// </summary>
-    public interface ISubmitable
+    public interface ISubmitable : IPointerClickHandler, ISubmitHandler
     {
         /// <summary>
         /// Event fired when this object is submitted.
