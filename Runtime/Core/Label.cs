87df315fe07f74695fc112348582baec727459b3
Hyago Oliveira <hyagogow@gmail.com>
2026-03-16 18:18:59 -0300

    Add Label component

---
+++ Runtime/Core/Label.cs
@@ -0,0 +1,53 @@
+using TMPro;
+using UnityEngine;
+
+namespace ActionCode.UISystem
+{
+    /// <summary>
+    /// Label component for Selectable UIs.
+    /// </summary>
+    [DisallowMultipleComponent]
+    public sealed class Label : MonoBehaviour
+    {
+        public TMP_Text target;
+        public SelectableColorData colors;
+
+        /// <summary>
+        /// The label text.
+        /// </summary>
+        public string Text
+        {
+            get => target.text;
+            set => target.text = value;
+        }
+
+        /// <summary>
+        /// The label color.
+        /// </summary>
+        public Color Color
+        {
+            get => target.color;
+            set => target.color = value;
+        }
+
+        public SelectableColorData Colors => colors;
+
+        private void Reset() => Setup();
+
+        public void FadeColor(Color color, float duration) => target.CrossFadeColor(
+            color,
+            duration,
+            ignoreTimeScale: true,
+            useAlpha: true
+        );
+
+        private void Setup()
+        {
+            target = GetComponent<TMP_Text>();
+            if (target == null) return;
+
+            target.color = Color.white;
+            target.raycastTarget = false;
+        }
+    }
+}
